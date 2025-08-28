21a27f7aa6d30d9c5da99b67adb9d89c82b338e9
Nathan Howell <nathan.d.howell@gmail.com>
2025-08-28 13:14:43 -0700

    Upgrade clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod lib;
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, Command};
 use std::error::Error;
 use std::fs::File;
 use std::io::{stdout, BufReader, BufWriter, Write};
 use std::{env, fs};
 
-fn app(name: &'_ str) -> App<'_> {
-    App::new(name)
+fn app(name: &'static str) -> Command {
+    Command::new(name)
         .version(crate_version!())
         .author(crate_authors!())
 }
@@ -15,20 +15,20 @@ fn run_app() -> Result<(), Box<dyn Error>> {
     let plan_arg = Arg::new("PLAN")
         .short('f')
         .long("file")
-        .takes_value(true)
+        .value_name("FILE")
         .default_value("cargo-plan.tar");
     let trailing_args = Arg::new("ARGS")
-        .multiple_values(true)
+        .num_args(0..)
         .last(true)
         .help("Additional arguments to pass to cargo");
     let matches = app(crate_name!())
         .about(crate_description!())
         .bin_name("cargo")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand_required(true)
         .subcommand(
             app("plan")
                 .about(crate_description!())
-                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand_required(true)
                 .subcommand(
                     app("create")
                         .about("Create a build plan")
@@ -52,34 +52,40 @@ fn run_app() -> Result<(), Box<dyn Error>> {
     match matches.subcommand() {
         Some(("plan", plan)) => match plan.subcommand() {
             Some(("create", args)) => {
-                let plan_path = args.value_of("PLAN").unwrap();
+                let plan_path = args.get_one::<String>("PLAN").unwrap();
                 let temp_dir = tempfile::TempDir::new()?;
                 let temp_file = temp_dir.path().join("plan.tar");
                 let archive = File::create(&temp_file)?;
                 let archive = BufWriter::new(archive);
                 let plan = lib::create(
                     env::current_dir()?,
-                    args.values_of_lossy("ARGS").unwrap_or_default(),
+                    args.get_many::<String>("ARGS")
+                        .unwrap_or_default()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<_>>(),
                     archive,
                 )?;
                 drop(plan);
                 fs::rename(&temp_file, plan_path)?;
             }
             Some(("build", args)) => {
-                let plan_path = args.value_of("PLAN").unwrap();
+                let plan_path = args.get_one::<String>("PLAN").unwrap();
                 let archive = File::open(plan_path)
                     .map_err(|e| format!("Failed to open {}: {}", plan_path, e))?;
                 let archive = BufReader::new(archive);
                 let working_directory = tempfile::TempDir::new()?;
                 lib::build(
                     working_directory.path(),
-                    args.values_of_lossy("ARGS").unwrap_or_default(),
+                    args.get_many::<String>("ARGS")
+                        .unwrap_or_default()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<_>>(),
                     archive,
                 )?;
             }
             Some(("generate-dockerfile", args)) => {
-                let dest = args.value_of("DEST").unwrap();
-                let archive = match dest {
+                let dest = args.get_one::<String>("DEST").unwrap();
+                let archive = match dest.as_str() {
                     "-" => Ok(Box::new(stdout()) as Box<dyn Write>),
                     _ => File::open(dest).map(|f| Box::new(f) as Box<dyn Write>),
                 }?;
